4e146eb1242e22491718dfbb8792694ffe2c66bb
uche-stan <uchenna_uso@yahoo.com>
2023-05-28 13:00:44 +0300

    updated mysql db and created booking and menu tables

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'db_name',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee123!'
     }
 }
 
